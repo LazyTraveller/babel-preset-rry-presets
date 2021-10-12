d78d89b7d5a3074ca47165c8311ee45725d1b7c2
heweizhi <heweizhi@guanmai.cn>
2021-10-12 11:16:39 +0800

    feat: presets

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    require("babel-preset-es2015"),
+    require("babel-preset-react")
+  ],
+  plugins: [
+    require("babel-plugin-transform-flow-strip-types")
+  ]
+};
